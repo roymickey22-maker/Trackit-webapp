705ebb9f293c1d48ca09fe47313cf0cbf266bbb6
Sundram Rai <skrai101.btech2023@ee.nitrr.ac.in>
2025-08-25 17:48:08 +0530

    Replace bcrypt with bcryptjs to fix native addon error

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jsonwebtoken from "jsonwebtoken";
 const jwt = jsonwebtoken;
 const userSchema = new mongoose.Schema(
